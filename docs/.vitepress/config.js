7182a54f23a7b5c6b1a26dade0170c2bc50f6a38
云云 <14131554+nononoGitee@user.noreply.gitee.com>
2025-01-04 23:01:59 +0800

    ✨ 添加404页面和站点配置优化

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,15 @@
 export default {
   title: 'SirenAI Docs',
   description: 'Documentation for SirenAI',
+  lang: 'zh-CN',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' }
     ],
-    sidebar: []
+    sidebar: [],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/yunzaixi-dev/sirenai-docs' }
+    ]
   },
   ignoreDeadLinks: true
 }
